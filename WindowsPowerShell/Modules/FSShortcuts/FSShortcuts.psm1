b6cc99474be05b1dfd054fc2b944ee4d3c9f89af
Andreas Eulitz <aeulitz@microsoft.com>
2019-08-28 13:44:04 -0700

    add FSShortcuts module

---
+++ WindowsPowerShell/Modules/FSShortcuts/FSShortcuts.psm1
@@ -0,0 +1,53 @@
+<#
+.SYNOPSIS
+	.
+
+.PARAMETER Text
+	.
+#>
+function FssEnsureShortcutDrive($driveLetter, $path)
+{
+	$substOut = &'subst'
+	if ($substOut -eq $null -or !$substOut.Contains($driveLetter))
+	{
+		subst $driveLetter $path
+	}
+}
+
+<#
+.SYNOPSIS
+	.
+
+.PARAMETER Text
+	.
+#>
+function FssExpandShortcut($shortcutPath)
+{
+	if (Test-Path "$shortcutPath\__ShortcutTarget.txt")
+	{
+		Invoke-Expression "`"$(Get-Content $shortcutPath\__ShortcutTarget.txt)`""
+	}
+	else
+	{
+		$shortcutPath
+	}
+}
+
+<#
+.SYNOPSIS
+	.
+
+.PARAMETER Text
+	.
+#>
+function FssSetLocationIfShortcut()
+{
+	if (Test-Path '__ShortcutTarget.txt')
+	{
+		cd (FssExpandShortcut (pwd))
+	}
+}
+
+Export-ModuleMember -Function FssEnsureShortcutDrive
+Export-ModuleMember -Function FssExpandShortcut
+Export-ModuleMember -Function FssSetLocationIfShortcut
