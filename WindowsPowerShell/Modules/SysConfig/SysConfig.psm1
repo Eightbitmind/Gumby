2a033e40bab109113b380f6e0b88f3a2c28a3780
Andreas Eulitz <aeulitz@microsoft.com>
2019-08-28 13:50:51 -0700

    add SysConfig module

---
+++ WindowsPowerShell/Modules/SysConfig/SysConfig.psm1
@@ -0,0 +1,29 @@
+using module Win32
+
+<#
+.SYNOPSIS
+	Switches mouse buttons.
+
+.PARAMETER Swap
+	.
+#>
+function SCSwapMouseButtons(<# $Swap = $true #>)
+{
+	Win32EnsureHelperFunctions
+
+	# requires a re-logon to make the changes effective
+	# get-itemproperty -Path "HKCU:\Control Panel\Mouse" -Name SwapMouseButtons
+
+
+	[bool] $wasSwapped = [Win32]::SwapMouseButton($true);
+	if ($wasSwapped)
+	{
+		# restore mouse button assignment
+		[Win32]::SwapMouseButton($false) | Out-Null
+	}
+
+
+	# [Win32]::SwapMouseButton($Swap)
+}
+
+Export-ModuleMember -Function SCSwapMouseButtons
