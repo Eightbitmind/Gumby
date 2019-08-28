8bd730f3e9375688715461c956a74489d00a7261
Andreas Eulitz <aeulitz@microsoft.com>
2019-08-28 14:12:58 -0700

    extend String module tests

--- WindowsPowerShell/Modules/String/TestString.ps1
+++ WindowsPowerShell/Modules/String/TestString.ps1
@@ -8,3 +8,15 @@ TestAreEqual (Abbreviate "abcdefghij" 7) "ab...ij"
 TestAreEqual (Abbreviate "abcdefghij" 6) "ab...j"
 TestAreEqual (Abbreviate "abcdefghij" 5) "a...j"
 TestAreEqual (Abbreviate "abcdefghij" 4) "a..."
+
+TestTuplesAreEqual (Split-Line "`"a`",`"b`",`"c`"") @('a', 'b', 'c')
+
+& {
+	$r = Zip ("a", "b", "c") (1, 2, 3)
+	TestAreEqual $r.Count 3
+	TestAreEqual $r.a 1
+	TestAreEqual $r.b 2
+	TestAreEqual $r.c 3
+}
+
+TestAreEqual (ConvertTo-NormalizedString "   a bc`tdef `t ghi `n") "a bc def ghi"
