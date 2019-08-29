459a6e5115d8f75de00b55246e2e9fa114f29064
Andreas Eulitz <aeulitz@outlook.com>
2019-08-29 01:08:35 -0700

    normalize Crypto module

--- WindowsPowerShell/Modules/Crypto/Crypto.psm1
+++ WindowsPowerShell/Modules/Crypto/Crypto.psm1
@@ -1,3 +1,13 @@
+<#
+.SYNOPSIS
+Gets MD5 hash for a string value.
+
+.PARAMETER Text
+String value to calculate MD5 hash for.
+
+.OUTPUTS
+Text representing MD5 hash.
+#>
 function GetMD5($Text) {
 	$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
 	$utf8 = New-Object -TypeName System.Text.UTF8Encoding
