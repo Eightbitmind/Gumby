2436907dab7a73d3ed14b5bca0ea464b0d804d1c
Andreas Eulitz <aeulitz@outlook.com>
2019-08-29 01:00:42 -0700

    normalize Assert module

--- WindowsPowerShell/Modules/Assert/Assert.psm1
+++ WindowsPowerShell/Modules/Assert/Assert.psm1
@@ -1,6 +1,15 @@
-function assert($cond, $message = "failed assertion")
-{
-	if (!$cond) { throw $message }
+<#
+.SYNOPSIS
+Asserts a condition.
+
+.PARAMETER Condition
+Value representing the condition.
+
+.PARAMETER Message
+Message to fail execution with if the condition is not true.
+#>
+function assert($Condition, $Message = "failed assertion") {
+	if (!$Condition) { throw $Message }
 }
 
 Export-ModuleMember -Function assert
