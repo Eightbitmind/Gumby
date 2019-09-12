8d0e1695ebb0addceaf77d50dc2011a62e5cbcae
Andreas Eulitz <aeulitz@outlook.com>
2019-09-12 00:14:23 -0700

    display class names during test execution

--- WindowsPowerShell/Modules/TestUtils/TestUtils.psm1
+++ WindowsPowerShell/Modules/TestUtils/TestUtils.psm1
@@ -107,17 +107,24 @@ class TestMethod : Attribute {}
 class TestMethodLogger : LogListenerBase {
 	[void] ProcessMessage([LogMessageType] $messageType, [string] $message) {
 		switch($messageType) {
+			([LogMessageType]::BeginSection) {
+				Write-Host $message
+				$this.indentation += 2
+			}
+			([LogMessageType]::EndSection) {
+				$this.indentation -= 2
+			}
 			([LogMessageType]::Warning) {
-				Write-Host -ForegroundColor Yellow $this.SpaceWords(($message, "WARNING"), [console]::WindowWidth, '.')
+				Write-Host -ForegroundColor Yellow ((' ' * $this.indentation) + $this.SpaceWords(($message, "WARNING"), [console]::WindowWidth - $this.indentation - 1, '.'))
 			}
 			([LogMessageType]::Error) {
-				Write-Host -ForegroundColor Red $this.SpaceWords(($message, "ERROR"), [console]::WindowWidth, '.')
+				Write-Host -ForegroundColor Red ((' ' * $this.indentation) + $this.SpaceWords(($message, "ERROR"), [console]::WindowWidth - $this.indentation - 1, '.'))
 			}
 			([LogMessageType]::Success) {
-				Write-Host -ForegroundColor Green $this.SpaceWords(($message, "SUCCESS"), [console]::WindowWidth, '.')
+				Write-Host -ForegroundColor Green ((' ' * $this.indentation) + $this.SpaceWords(($message, "SUCCESS"), [console]::WindowWidth - $this.indentation - 1, '.'))
 			}
 			([LogMessageType]::Failure) {
-				Write-Host -ForegroundColor Red $this.SpaceWords(($message, "FAILURE"), [console]::WindowWidth, '.')
+				Write-Host -ForegroundColor Red ((' ' * $this.indentation) + $this.SpaceWords(($message, "FAILURE"), [console]::WindowWidth - $this.indentation - 1, '.'))
 			}
 		}
 	}
@@ -145,6 +152,8 @@ class TestMethodLogger : LogListenerBase {
 
 		return $sb.ToString()
 	}
+
+	hidden [int] $indentation = 0
 }
 
 class TestRunner {
@@ -165,6 +174,7 @@ class TestRunner {
 	}
 
 	hidden [void] RunTestClass($testClass) {
+
 		try {
 			$testClassInstance = $testClass::new()
 		} catch {
@@ -172,86 +182,90 @@ class TestRunner {
 			return
 		}
 
+		$this.TestMethodLogger.ProcessMessage(([LogMessageType]::BeginSection), $testClass.Name)
 		[Log]::BeginSection("test class `"$($testClass.Name)`"")
-		
-		$testClassSetupMethodsSucceeded = $true
-		foreach ($classSetupMethod in $this.FindMembers($testClass, ([TestClassSetup]))) {
-			try {
-				$classSetupMethod.Invoke($testClassInstance, @())
-			} catch {
-				[Log]::Error("failed to execute `"$($classSetupMethod.Name)`" class setup method of test class `"$($testClass.Name)`"")
-	
-				# don't execute any test methods
-				$testClassSetupMethodsSucceeded = $false
-	
-				# stop executing other class setup methods
-				break
-			}
-		}
-	
-		if ($testClassSetupMethodsSucceeded) {
-			foreach ($testMethod in $this.FindMembers($testClass, ([TestMethod]))) {
-	
-				# helper object that allows us to capture the counts "by reference"
-				$testMethodStats = @{
-					WarningCount = 0
-					ErrorCount = 0
-					SuccessCount = 0
-					FailureCount = 0
-				}
-	
-				$lo = [LogObserver]::new({
-					param($mt, $m)
-					switch ($mt) {
-						([LogMessageType]::Warning) { ++$testMethodStats.WarningCount }
-						([LogMessageType]::Error) { ++$testMethodStats.ErrorCount }
-						([LogMessageType]::Success) { ++$testMethodStats.SuccessCount }
-						([LogMessageType]::Failure) { ++$testMethodStats.FailureCount }
-					}
-				})
-	
-				$testMethodException = $null
-				[Log]::Listeners.Add($lo) | Out-Null
-	
-				[Log]::BeginSection("test method `"$($testMethod.Name)`"")
 
+		try {
+			$testClassSetupMethodsSucceeded = $true
+			foreach ($classSetupMethod in $this.FindMembers($testClass, ([TestClassSetup]))) {
 				try {
-					$testMethod.Invoke($testClassInstance, @())
+					$classSetupMethod.Invoke($testClassInstance, @())
 				} catch {
-					$testMethodException = $_.Exception.InnerException
+					[Log]::Error("failed to execute `"$($classSetupMethod.Name)`" class setup method of test class `"$($testClass.Name)`"")
+		
+					# don't execute any test methods
+					$testClassSetupMethodsSucceeded = $false
+		
+					# stop executing other class setup methods
+					break
 				}
-
-				[Log]::EndSection("test method `"$($testMethod.Name)`"")
-
-				[Log]::Listeners.Remove($lo)
-	
-				if ($testMethodException -ne $null) {
-					[Log]::Error("test method `"$($testMethod.Name)`" failed due to exception `"$($testMethodException.Message)`"")
-					$this.TestMethodLogger.ProcessMessage(([LogMessageType]::Error), $testMethod.Name)
-				} elseif($testMethodStats.ErrorCount -gt 0) {
-					$this.TestMethodLogger.ProcessMessage(([LogMessageType]::Error), $testMethod.Name)
-				} elseif (($testMethodStats.FailureCount -eq 0) -and ($testMethodStats.SuccessCount -gt 0)) {
-					if ($testMethodStats.WarningCount -eq 0) {
-						$this.TestMethodLogger.ProcessMessage(([LogMessageType]::Success), $testMethod.Name)
-					} else {
-						$this.TestMethodLogger.ProcessMessage(([LogMessageType]::Warning), $testMethod.Name)
+			}
+		
+			if ($testClassSetupMethodsSucceeded) {
+				foreach ($testMethod in $this.FindMembers($testClass, ([TestMethod]))) {
+		
+					# helper object that allows us to capture the counts "by reference"
+					$testMethodStats = @{
+						WarningCount = 0
+						ErrorCount = 0
+						SuccessCount = 0
+						FailureCount = 0
 					}
-				} else {
-					$this.TestMethodLogger.ProcessMessage(([LogMessageType]::Failure), $testMethod.Name)
+		
+					$lo = [LogObserver]::new({
+						param($mt, $m)
+						switch ($mt) {
+							([LogMessageType]::Warning) { ++$testMethodStats.WarningCount }
+							([LogMessageType]::Error) { ++$testMethodStats.ErrorCount }
+							([LogMessageType]::Success) { ++$testMethodStats.SuccessCount }
+							([LogMessageType]::Failure) { ++$testMethodStats.FailureCount }
+						}
+					})
+		
+					$testMethodException = $null
+					
+					[Log]::BeginSection("test method `"$($testMethod.Name)`"")
+					
+					[Log]::Listeners.Add($lo) | Out-Null
+					try {
+						$testMethod.Invoke($testClassInstance, @())
+					} catch {
+						$testMethodException = $_.Exception.InnerException
+					}
+					[Log]::Listeners.Remove($lo)
+		
+					if ($testMethodException -ne $null) {
+						[Log]::Error("exception `"$($testMethodException.Message)`"")
+						$this.TestMethodLogger.ProcessMessage(([LogMessageType]::Error), $testMethod.Name)
+					} elseif($testMethodStats.ErrorCount -gt 0) {
+						$this.TestMethodLogger.ProcessMessage(([LogMessageType]::Error), $testMethod.Name)
+					} elseif (($testMethodStats.FailureCount -eq 0) -and ($testMethodStats.SuccessCount -gt 0)) {
+						if ($testMethodStats.WarningCount -eq 0) {
+							$this.TestMethodLogger.ProcessMessage(([LogMessageType]::Success), $testMethod.Name)
+						} else {
+							$this.TestMethodLogger.ProcessMessage(([LogMessageType]::Warning), $testMethod.Name)
+						}
+					} else {
+						$this.TestMethodLogger.ProcessMessage(([LogMessageType]::Failure), $testMethod.Name)
+					}
+
+					[Log]::EndSection("test method `"$($testMethod.Name)`"")
 				}
 			}
-		}
-	
-		foreach ($classTeardownMethod in $this.FindMembers($testClass, ([TestClassTeardown]))) {
-			try {
-				$classTeardownMethod.Invoke($testClassInstance, @())
-			} catch {
-				[Log]::Error("failed to execute `"$($classTeardownMethod.Name)`" class teardown method of test class `"$($testClass.Name)`"")
-				# continue executing other class teardown methods
+		
+			foreach ($classTeardownMethod in $this.FindMembers($testClass, ([TestClassTeardown]))) {
+				try {
+					$classTeardownMethod.Invoke($testClassInstance, @())
+				} catch {
+					[Log]::Error("failed to execute `"$($classTeardownMethod.Name)`" class teardown method of test class `"$($testClass.Name)`"")
+					# continue executing other class teardown methods
+				}
 			}
-		}
 
-		[Log]::EndSection("test class `"$($testClass.Name)`"")
+		} finally {
+			[Log]::EndSection("test class `"$($testClass.Name)`"")
+			$this.TestMethodLogger.ProcessMessage(([LogMessageType]::EndSection), $testClass.Name)
+		}
 	}
 
 	hidden [Reflection.MemberInfo[]] FindMembers([Reflection.TypeInfo] $testClass, $attributeType) {
