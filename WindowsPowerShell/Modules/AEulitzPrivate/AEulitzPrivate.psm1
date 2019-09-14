e7591231460a2f524a4f47cba4bfafecc4b2e6f9
Andreas Eulitz <aeulitz@microsoft.com>
2019-09-13 18:59:39 -0700

    update private shortcuts

--- WindowsPowerShell/Modules/AEulitzPrivate/AEulitzPrivate.psm1
+++ WindowsPowerShell/Modules/AEulitzPrivate/AEulitzPrivate.psm1
@@ -1,8 +1,16 @@
+using module SysConfig
+
 function AEulitzPrivateShortcuts() {
 	return @{
 		"Directories" = @{
 			"ToolsGitHub"              = { Set-Location "$HOME/ToolsGitHub" }
 			"ToolsGitHub - PS Modules" = { Set-Location "$HOME/ToolsGitHub/WindowsPowerShell/Modules" }
+		}
+		"Websites" = @{
+			"ToolsGitHub"     = { start 'https://github.com/Eightbitmind/tools'}
+		}
+		"Commands" = @{
+			"Swap Mouse Buttons" = { SCSwapMouseButtons }
 		}
 	}
 }
