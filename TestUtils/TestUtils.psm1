02ae2848b2dabdcc978b7e8bab586d1d215124a1
Andreas Eulitz <aeulitz@outlook.com>
2020-02-10 23:03:26 -0800

    restore TestUtil.psm1

--- TestUtils/TestUtils.psm1
+++ TestUtils/TestUtils.psm1
@@ -1,100 +1,341 @@
-using module Log
-using module String
+using module Gumby.Log
+using module Gumby.String
 
-function TestIsTrue($condition, $message = "value is true") {
-	if ($condition) {
-		[Log]::Success($message)
-	} else {
-		[Log]::Failure($message)
+#region Expectations
+
+# An expectation can be seen as a function with two parameters
+#     function IsMatch($expected, $actual)
+# which has been curried into a function with one parameter
+#     function IsMatch($actual)
+# by capturing the argument for the $expected parameter. Expectation trees - built, for instance,
+# via the constructor arguments of the And and Or expectations - allow for the validation of
+# complex test conditions. For instance, the expectation tree
+#     (And (ListContains 'a') (Not (ListContains 'b')))
+# checks that an actual list data structure contains an element 'a', but no element 'b'. The
+# IsMatch function below treats native PowerShell values and objects as expectations. For instance,
+# the integer 1 is treated as an expectation checking that an actual value equals 1; the array
+# @(1, 2, 3) is treated as an expectation checking that an actual object yields the values 1, 2 and
+# 3 when iterated; the object @{Name = "Joe"; Age = 33} checks that an actual object has a
+# member "Name" with the value "Joe" and a member "Age" with the value 33.
+
+class ExpectationBase {
+	[bool] IsMatch($actual, $messagePrefix) {
+		throw "derived classes must implement this method"
 	}
 }
 
-function TestIsFalse($condition, $message = "value is false") {
-	if (!$condition) {
-		[Log]::Success($message)
-	} else {
-		[Log]::Failure($message)
+class CustomExpectation : ExpectationBase {
+	CustomExpectation($name, $predicate) {
+		$this.name = $name
+		$this.predicate = $predicate
 	}
-}
 
-function TestIsNull($actual, $message = "object is null") {
-	if ($null -eq $actual) {
-		[Log]::Success($message)
-	} else {
-		[Log]::Failure($message)
-	}
-}
-
-function TestIsNotNull($actual, $message = "object is not null") {
-	if ($null -ne $actual) {
-		[Log]::Success($message)
-	} else {
-		[Log]::Failure($message)
-	}
-}
-
-function TestIsType($object, $type) {
-	if ($object -is $type) {
-		[Log]::Success("object is of type $($type.Name)")
-	} else {
-		[Log]::Failure("object is of type $($object.GetType()), expected $($type.Name)")
-	}
-}
-
-function TestAreEqual($actual, $expected, $message = "Test Value") {
-	if ($actual -eq $expected) {
-		[Log]::Success("value '$actual' matches expectation")
-	} else {
-		[Log]::Failure("actual '$($actual)', expected '$($expected)'")
-	}
-}
-
-function TestTuplesAreEqual($actual, $expected, $message = "Test Tuples") {
-	$actualEnum = $actual.GetEnumerator()
-	$expectedEnum = $expected.GetEnumerator()
-
-	while ($actualEnum.MoveNext()) {
-		if (!$expectedEnum.MoveNext()) {
-			[Log]::Failure("$($message): more items than expected")
-		}
-
-		if ($actualEnum.Current -eq $expectedEnum.Current) {
-			[Log]::Success("$($message): found expected item '$($expectedEnum.Current)'")
+	[bool] IsMatch($actual, $messagePrefix) {
+		if ($this.predicate.Invoke($actual)) {
+			[Log]::Success("$($messagePrefix)`"$($this.name)`" expectation matches")
+			return $true
 		} else {
-			[Log]::Failure("$($message): actual '$($actualEnum.Current)', expected '$($expectedEnum.Current)'")
+			[Log]::Failure("$($messagePrefix)`"$($this.name)`" expectation does not match")
+			return $false
 		}
 	}
 
-	if ($expectedEnum.MoveNext()) {
-		[Log]::Failure("$($message): fewer items than expected")
-	}
-
-	[Log]::Success("$($message): found expected items")
+	hidden [string] $name
+	hidden [scriptblock] $predicate
 }
 
-function TestTuplesMatch($actual, $expected, $message = "Test Tuples") {
-	$actualEnum = $actual.GetEnumerator()
-	$expectedEnum = $expected.GetEnumerator()
+function Expect($name, $expected) { [CustomExpectation]::new($name, $expected) }
 
-	while ($actualEnum.MoveNext()) {
-		if (!$expectedEnum.MoveNext()) {
-			# Write-Host -ForegroundColor Red "$($message): more items than expected"
-			[Log]::Failure("$($message): more items than expected")
+<#
+This expectation could be encoded in many other ways, e.g.
+	(ExpectNot $null)
+It is nevertheless useful because it provides clearer logging.
+#>
+class NotNullExpectation : ExpectationBase {
+	[bool] IsMatch($actual, $messagePrefix) {
+		if ($actual -ne $null) {
+			[Log]::Success("$($messagePrefix)object '$actual' is not null")
+			return $true
+		} else {
+			[Log]::Failure("$($messagePrefix)object is null")
+			return $false
 		}
+	}
+}
 
-		if ($actualEnum.Current -notmatch $expectedEnum.Current) {
-			# Write-Host -ForegroundColor Red "$($message): actual '$($actualEnum.Current)', expected '$($expectedEnum.Current)'"
-			[Log]::Failure("$($message): actual '$($actualEnum.Current)', expected '$($expectedEnum.Current)'")
+function ExpectNotNull { [NotNullExpectation]::new() }
+
+class RegexExpectation : ExpectationBase {
+	RegexExpectation($expectedPattern) { $this.expectedPattern = $expectedPattern }
+
+	[bool] IsMatch($actual, $messagePrefix) {
+		if ($actual -match $this.expectedPattern) {
+			[Log]::Success("$($messagePrefix)`"$actual`" matches pattern `"$($this.pattern)`"")
+			return $true
+		} else {
+			[Log]::Failure("$($messagePrefix)`"$actual`" does not match pattern `"$($this.pattern)`"")
+			return $false
 		}
 	}
 
-	if ($expectedEnum.MoveNext()) {
-		# Write-Host -ForegroundColor Red "$($message): fewer items than expected"
-		[Log]::Failure("$($message): fewer items than expected")
+	hidden [string] $expectedPattern
+}
+
+function ExpectRegex($pattern) { [RegexExpectation]::new($pattern) }
+
+class TypeExpectation : ExpectationBase {
+	TypeExpectation($expectedType) { $this.expectedType = $expectedType }
+
+	[bool] IsMatch($actual, $messagePrefix) {
+		if ($actual -is $this.expectedType) {
+			[Log]::Success("$($messagePrefix)object is of expected type $($this.expectedType.Name)")
+			return $true
+		} else {
+			[Log]::Failure("$($messagePrefix)object is of type $($actual.GetType()), expected $($this.expectedType.Name)")
+			return $false
+		}
 	}
 
-	# Write-Host -ForegroundColor Green "$($message): found expected items"
-	[Log]::Success("$($message): found expected items")
+	hidden [System.Reflection.TypeInfo] $expectedType
+}
+
+function ExpectType($type) { [TypeExpectation]::new($type) }
+
+class KeyCountEqualExpectation : ExpectationBase {
+
+	KeyCountEqualExpectation($expectedKeyCount) { $this.expectedKeyCount = $expectedKeyCount }
+
+	[bool] IsMatch($actual, $messagePrefix) {
+		if ($actual.Keys.Count -eq $this.expectedKeyCount) {
+			[Log]::Success("$($messagePrefix)object key count $($this.expectedKeyCount) as expected")
+			return $true
+		} else {
+			[Log]::Failure("$($messagePrefix)actual object key count $($actual.Keys.Count), expected $($this.expectedKeyCount)")
+			return $false
+		}
+	}
+
+	hidden [uint32] $expectedKeyCount
+}
+
+function ExpectKeyCountEqual($count) { [KeyCountEqualExpectation]::new($count) }
+
+#region List Expectations
+
+class ContainsExpectation : ExpectationBase {
+	ContainsExpectation($expectedItem) { $this.expectedItem = $expectedItem }
+
+	[bool] IsMatch($actualItems, $messagePrefix) {
+
+		# We're performing an opportunistic search below where a non-matching actual item does not
+		# necessarily constitute a failure.
+		$logInterceptor = [LogInterceptor]::new({param($interceptor, $messageType, $message)
+			if ($messageType -eq ([LogMessageType]::Success)) {
+				$interceptor.DispatchMessage($messageType, $message)
+			}
+		})
+
+		try {
+			$actualItemIndex = 0
+			foreach ($actualItem in $actualItems) {
+				if (IsMatch $this.expectedItem $actualItem "$($messagePrefix)item $(($actualItemIndex++)): ") {
+					[Log]::Failure("$($messagePrefix)found expected item '$($this.expectedItem)'")
+					return $true
+				}
+			}
+
+		} finally {
+			$logInterceptor.Dispose()
+		}
+
+		[Log]::Failure("$($messagePrefix)missing item '$($this.expectedItem)'")
+		return $false
+	}
+
+	hidden $expectedItem
+}
+
+function ExpectContains($item) { [ContainsExpectation]::new($item) }
+
+class CountGreaterOrEqualExpectation : ExpectationBase {
+	CountGreaterOrEqualExpectation($expectedCount) {
+		$this.expectedCount = $expectedCount
+	}
+
+	[bool] IsMatch($actual, $messagePrefix) {
+		$actualCount = $actual.Count
+		if ($actualCount -ge $this.expectedCount) {
+			[Log]::Success("$($messagePrefix)item count $($this.expectedCount) is greater or equal to $actualCount")
+			return $true
+		} else {
+			[Log]::Failure("$($messagePrefix)item count $($this.expectedCount) is less than $actualCount")
+			return $false
+		}
+	}
+
+	hidden $expectedCount
+}
+
+function ExpectCountGreaterOrEqual($count) { [CountGreaterOrEqualExpectation]::new($count) }
+
+# Future: ExpectCountEquals, ExpectEachItem (iterates over the items in the actual data structure,
+# passing each into child expectations)
+
+#endregion
+
+#region Boolean Expectations
+
+class NotExpectation : ExpectationBase {
+	NotExpectation($expected) { $this.expected = $expected }
+
+	[bool] IsMatch($actual, $messagePrefix) {
+		$logInterceptor = [LogInterceptor]::new({ param ($interceptor, $messageType, $message)
+			switch ($messageType) {
+				([LogMessageType]::Failure) { $messageType = ([LogMessageType]::Success)}
+				([LogMessageType]::Success) { $messageType = ([LogMessageType]::Failure)}
+			}
+			$interceptor.DispatchMessage($messageType, $message)
+		})
+		try {
+			return !(IsMatch $this.expected $actual "$($messagePrefix)NOT: ")
+		} finally {
+			$logInterceptor.Dispose()
+		}
+	}
+
+	hidden $expected
+}
+
+function ExpectNot($operand) { [NotExpectation]::new($operand) }
+
+class AndExpectation : ExpectationBase {
+	AndExpectation($expected) { $this.expected  = $expected }
+
+	[bool] IsMatch($actual, $messagePrefix) {
+		foreach ($expectedTerm in $this.expected) {
+			if (!(IsMatch $expectedTerm $actual "$($messagePrefix)AND: ")) {
+				[Log]::Failure("$($messagePrefix)AND evaluates to false")
+				return $false
+			}
+		}
+		[Log]::Success("$($messagePrefix)AND evaluates to true")
+		return $true
+	}
+
+	hidden $expected
+}
+
+function ExpectAnd { [AndExpectation]::new($args) }
+
+class OrExpectation : ExpectationBase {
+	OrExpectation($expected) { $this.expected  = $expected }
+
+	[bool] IsMatch($actual, $messagePrefix) {
+		# A false OR term does not necessarily constitute a failure.
+		$logInterceptor = [LogInterceptor]::new({param($interceptor, $messageType, $message)
+			if ($messageType -eq ([LogMessageType]::Failure)) {
+				$messageType = ([LogMessageType]::Comment)
+			}
+			$interceptor.DispatchMessage($messageType, $message)
+		})
+
+		$result = $false
+		try {
+			foreach ($expectedTerm in $this.expected) {
+				if (IsMatch $expectedTerm $actual "$($messagePrefix)OR: ") {
+					$result = $true
+					break
+				}
+			}
+		} finally {
+			$logInterceptor.Dispose()
+		}
+
+		if ($result) {
+			[Log]::Success("$($messagePrefix)OR evaluates to true")
+		} else {
+			[Log]::Failure("$($messagePrefix)OR evaluates to false")
+		}
+		return $result
+	}
+
+	hidden $expected
+}
+
+function ExpectOr { [OrExpectation]::new($args) }
+
+#endregion
+
+#endregion
+
+function IsMatch($expected, $actual, $messagePrefix) {
+
+	function AreValuesEqual($expected, $actual, $messagePrefix) {
+		if ($expected -eq $null) {
+	
+			if ($actual -eq $null) {
+				[Log]::Success("$($messagePrefix)value is null")
+				return $true
+			} else {
+				[Log]::Failure("$($messagePrefix)value is not null")
+				return $false
+			}
+	
+		} elseif ($actual -eq $expected) {
+			[Log]::Success("$($messagePrefix)value '$actual' matches expectation")
+			return $true
+		} else {
+			[Log]::Failure("$($messagePrefix)actual '$($actual)', expected '$($expected)'")
+			return $false
+		}
+	}
+
+	if ($expected -is [ExpectationBase]) {
+		return $expected.IsMatch($actual, $messagePrefix)
+	} elseif (($expected -eq $null) -or ($expected -is [bool]) -or ($expected -is [int]) -or ($expected -is [string])) {
+		return (AreValuesEqual $expected $actual $messagePrefix)
+	} elseif ($expected -is [array]) {
+		# Implementation of System.Collections.IEnumerable cannot be used to differentiate between
+		# objects and arrays as both implement this interface.
+
+		$result = $true
+		$actualEnum = $actual.GetEnumerator()
+		$expectedEnum = $expected.GetEnumerator()
+		$itemIndex = 0
+
+		while ($actualEnum.MoveNext()) {
+			if (!$expectedEnum.MoveNext()) {
+				[Log]::Failure("$($messagePrefix)more items than expected")
+				return $false
+			}
+
+			$result = $result -and (IsMatch $expectedEnum.Current $actualEnum.Current "$($messagePrefix)item $(($itemIndex++)): ")
+		}
+
+		if ($expectedEnum.MoveNext()) {
+			[Log]::Failure("$($messagePrefix)fewer items than expected")
+			return $false
+		}
+
+		return $result
+	} elseif ($expected -is [object]) {
+
+		$result = $true
+
+		foreach ($key in $expected.Keys) {
+			$expectedValue = $expected[$key]
+			$actualValue = $actual.($key)
+			$result = $result -and (IsMatch $expectedValue $actualValue "$($messagePrefix)member '$key': ")
+		}
+
+		return $result
+	}
+}
+
+# The way our test executor works, we do not need the return value suppression provided by this
+# function. At this point, this function is merely syntactic sugar.
+function Test($Expected, $Actual, $MessagePrefix) {
+	[void](IsMatch $Expected $Actual $MessagePrefix)
 }
 
 class TestClass : Attribute {}
