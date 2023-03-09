d524162357ead2494b26b3ab8ab623c43c307b93
Andreas Eulitz <aeulitz@outlook.com>
2023-03-08 19:51:08 -0800

    PS7ify File module, add test

---
+++ File/TestModule.ps1
@@ -0,0 +1,49 @@
+using module Gumby.Debug
+using module Gumby.Test
+using module ".\File.psm1"
+
+param([ValidateSet("ExportTests", "RunTests")] $Mode = "RunTests")
+
+[TestClass()]
+class GetTextFileEncodingTests {
+
+	[TestClassSetup()]
+	[void] Setup() {
+		$testFolderBaseName = "$env:TEMP\GetTextFileEncodingTests"
+		$this.testFolder = $testFolderBaseName
+		$success = $false
+		for ($i = 2; $i -lt <# sanity cap #> 1000; ++$i) {
+			if (!(Test-Path $this.testFolder)) {
+				mkdir $this.testFolder
+				$success = $true
+				break
+			}
+			$this.testFolder = "$testFolderBaseName-{0:D3}" -f $i
+		}
+		Assert $success
+	}
+
+	[TestClassTeardown()]
+	[void] Teardown() {
+		Remmove-Item -Recurse -Force $this.testFolder
+	}
+
+	[TestMethod()]
+	[void] GetTextFileEncodingASCII() {
+		$testFile = "$($this.testFolder)\ascii.txt"
+		[void](Out-File -Encoding ascii -Path $testFile -InputObject "The quick brown fox")
+
+		Test ([TextFileEncoding]::ASCII) (Get-TextFileEncoding $testFile)
+		# Write-Host (Get-TextFileEncoding $testFile)
+		# Write-Host ([TextFileEncoding]::ASCII)
+		#Test 1 1
+	}
+
+	[string] $testFolder
+}
+
+$tests = @([GetTextFileEncodingTests])
+switch ($Mode) {
+	"ExportTests" { $tests }
+	"RunTests" { RunTests "$env:TEMP\StringTests.log" @tests }
+}
