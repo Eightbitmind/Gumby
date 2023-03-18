095f5de42224e8755e636be08a5448c02b374a77
Andreas Eulitz <aeulitz@outlook.com>
2023-03-17 18:12:24 -0700

    adding conversion functions

--- Conversion/Conversion.psm1
+++ Conversion/Conversion.psm1
@@ -1,4 +1,7 @@
 function Convert-ToNearestFrac([double] $Number, [int] $Denominator) {
+	# Future: Denominator should have a default value of 0 which - as an impossible denominator -
+	# could indicate that the resulting fraction should be shortened.
+
 	# (8.25, 16) => 8 4/16
 	# TODO: shorten the fraction
 	[int] $whole = [Math]::Floor($Number)
@@ -8,22 +11,33 @@ function Convert-ToNearestFrac([double] $Number, [int] $Denominator) {
 
 #region Length
 
-function Convert-InchesToMeters([double] $Inches, [int] $Precision = -1, [switch] $IncludeUnit) {
+#region Metric to US
 
-	[double] $meters = $Inches * 0.0254
-
-	if ($Precision -ge 0) {
-		$meters = [Math]::Round($meters, $Precision)
-	}
-
-	if ($IncludeUnit) {
-		return "$meters m" # return value is a string
-	} else {
-		return $meters # return value is a double
-	}
-
+function Convert-CentimetersToInches([double] $Centimeters, [int] $Precision = -1, [switch] $IncludeUnit) {
+	return (Convert-MetersToInches -Meters ($Centimeters / 100) -Precision $Precision, -IncludeUnit $IncludeUnit)
 }
 
+function Convert-CentimetersToInchesFrac([double] $Centimeters, [int] $Denominator = 16) {
+	return (Convert-MetersToInchesFrac -Meters ($Centimeters / 100) -Denominator $Denominator)
+}
+
+<#
+.SYNOPSIS
+TBW
+
+.PARAMETER Meters
+TBW
+
+.PARAMETER Denominator
+TBW
+
+.OUTPUTS
+TBW
+
+.NOTES
+This function should be the only one containing the conversion factor between a metric length and
+inches.
+#>
 function Convert-MetersToInches([double] $Meters, [int] $Precision = -1, [switch] $IncludeUnit) {
 
 	[double] $inches = $Meters / 0.0254
@@ -39,20 +53,29 @@ function Convert-MetersToInches([double] $Meters, [int] $Precision = -1, [switch
 	}
 }
 
-function Convert-FeetToMeters([double] $Feet, [int] $Precision = -1, [switch] $IncludeUnit) {
+<#
+.SYNOPSIS
+TBW
 
-	[double] $meters = $Feet * 0.3048
+.PARAMETER Meters
+TBW
 
-	if ($Precision -ge 0) {
-		$meters = [Math]::Round($meters, $Precision)
-	}
+.PARAMETER Denominator
+TBW
 
-	if ($IncludeUnit) {
-		return "$meters m" # return value is a string
-	} else {
-		return $meters # return value is a double
-	}
+.OUTPUTS
+String consisting of whole and fractional inches.
 
+.NOTES
+By its nature, the output is a string (it cannot be a numeric value without subverting the "Frac"
+semantics). Therefore, a Precision parameter makes no sense for this function.
+#>
+function Convert-MetersToInchesFrac([double] $Meters, [int] $Denominator = 16) {
+	[double] $inches = Convert-MetersToInches $Meters
+
+	[string] $inchesFrac = Convert-ToNearestFrac $inches $Denominator
+
+	Write-Output "$inchesFrac"
 }
 
 function Convert-MetersToFeet([double] $Meters, [int] $Precision = -1, [switch] $IncludeUnit) {
@@ -77,6 +100,44 @@ function Convert-MetersToFeetInchesFrac([double] $Meters, [int] $Denominator = 1
 	$inches = Convert-ToNearestFrac (Convert-MetersToInches $remainder) $Denominator
 	Write-Output "$feet' $inches`""
 }
+
+#endregion
+
+#region US to Metric
+
+function Convert-InchesToMeters([double] $Inches, [int] $Precision = -1, [switch] $IncludeUnit) {
+
+	[double] $meters = $Inches * 0.0254
+
+	if ($Precision -ge 0) {
+		$meters = [Math]::Round($meters, $Precision)
+	}
+
+	if ($IncludeUnit) {
+		return "$meters m" # return value is a string
+	} else {
+		return $meters # return value is a double
+	}
+
+}
+
+function Convert-FeetToMeters([double] $Feet, [int] $Precision = -1, [switch] $IncludeUnit) {
+
+	[double] $meters = $Feet * 0.3048
+
+	if ($Precision -ge 0) {
+		$meters = [Math]::Round($meters, $Precision)
+	}
+
+	if ($IncludeUnit) {
+		return "$meters m" # return value is a string
+	} else {
+		return $meters # return value is a double
+	}
+
+}
+
+#endregion
 
 #endregion
 
