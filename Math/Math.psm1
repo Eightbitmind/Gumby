c2c629fdb733b59a3b2e5128f38d57beff95292b
Andreas Eulitz <aeulitz@outlook.com>
2023-08-16 15:21:50 -0700

    add EncodeBase26, GenerateRandomLetterId

--- Math/Math.psm1
+++ Math/Math.psm1
@@ -11,3 +11,77 @@ True if the number is even, false otherwise.
 function IsEven([int] $n) {
 	return $n % 2 -eq 0
 }
+
+<#
+.SYNOPSIS
+Encode a number in a base-26 system using Latin uppercase letters as digits.
+
+.PARAMETER n
+Number to encode.
+
+.OUTPUTS
+Base-26 encoding of number.
+
+.EXAMPLE
+PS> EncodeBase26 0
+A
+
+.EXAMPLE
+PS> EncodeBase26 25
+Z
+
+.EXAMPLE
+PS> EncodeBase26 26
+BA
+
+.EXAMPLE
+PS> EncodeBase26 77
+CZ
+
+.EXAMPLE
+PS> EncodeBase26 1234
+BVM
+#>
+function EncodeBase26($n) {
+	function ToChar([int]$digit) { return [char](([int]'A'[0]) + $digit) }
+
+	$base = 26
+	$remainder = $n
+	$result = "";
+
+	while ($true) {
+		$digit = $remainder % $base
+		$remainder = ($remainder - $digit) / $base
+		$result = (ToChar $digit) + $result
+		if ($remainder -eq 0) {break}
+	}
+
+	return $result
+}
+
+<#
+.SYNOPSIS
+Generate a random string consisting of uppercase Latin letters.
+
+.PARAMETER Length
+Length of string to generate (defaults to 3).
+
+.OUTPUTS
+Random string.
+
+.EXAMPLE
+PS> GenerateRandomLetterId
+LGW
+
+.EXAMPLE
+PS> GenerateRandomLetterId -Length 1
+T
+
+.EXAMPLE
+PS> GenerateRandomLetterId -Length 4
+SZCW
+#>
+function GenerateRandomLetterId($Length = 3)
+{
+	return (EncodeBase26 (Get-Random -Maximum ([math]::pow(26, $Length))))
+}
--- Math/TestModule.ps1
+++ Math/TestModule.ps1
@@ -5,34 +5,80 @@ param([ValidateSet("ExportTests", "RunTests")] $Mode = "RunTests")
 Import-Module "$PSScriptRoot/Math.psm1"
 
 [TestClass()]
-class MathModuleTests {
+class IsEvenTests {
 	[TestMethod()]
-	[void] IsEven_m2() {
+	[void] Minus2_IsEven() {
 		Test $true (IsEven -2)
 	}
 
 	[TestMethod()]
-	[void] IsEven_m1() {
+	[void] Minus1_IsNotEven() {
 		Test $false (IsEven -1)
 	}
 
 	[TestMethod()]
-	[void] IsEven_0() {
+	[void] Zero_IsEven() {
 		Test $true (IsEven 0)
 	}
 
 	[TestMethod()]
-	[void] IsEven_p1() {
+	[void] One_IsNotEven() {
 		Test $false (IsEven 1)
 	}
 
 	[TestMethod()]
-	[void] IsEven_p2() {
+	[void] Two_IsEven() {
 		Test $true (IsEven 2)
 	}
 }
 
-$tests = ([MathModuleTests])
+[TestClass()]
+class EncodeB26Tests {
+	[TestMethod()]
+	[void] EncodeB26_0_A() {
+		Test "A" (EncodeBase26 0)
+	}
+
+	[TestMethod()]
+	[void] EncodeB26_25_Z() {
+		Test "Z" (EncodeBase26 25)
+	}
+
+	[TestMethod()]
+	[void] EncodeB26_26_BA() {
+		Test "BA" (EncodeBase26 26)
+	}
+
+	[TestMethod()]
+	[void] EncodeB26_77_CZ() {
+		Test "CZ" (EncodeBase26 77)
+	}
+
+	[TestMethod()]
+	[void] EncodeB26_1234_BVM() {
+		Test "BVM" (EncodeBase26 1234)
+	}
+}
+
+[TestClass()]
+class GenerateRandomLetterIdTests {
+	[TestMethod()]
+	[void] DefaultLength_Generates3CharString() {
+		Test 3 (GenerateRandomLetterId).Length
+	}
+
+	[TestMethod()]
+	[void] Length1_Generates1CharString() {
+		Test 1 (GenerateRandomLetterId -Length 1).Length
+	}
+
+	[TestMethod()]
+	[void] Length4_Generates4CharString() {
+		Test 4 (GenerateRandomLetterId -Length 4).Length
+	}
+}
+
+$tests = ([IsEvenTests], [EncodeB26Tests], [GenerateRandomLetterIdTests])
 switch ($Mode) {
 	"ExportTests" { $tests }
 	"RunTests" { RunTests "$env:TEMP\MathTests.log" @tests }
